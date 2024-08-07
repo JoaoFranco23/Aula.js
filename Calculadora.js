e81c619b918bf4032d65dd9d3a6da954be15a226
JoaoFranco23 <147751659+JoaoFranco23@users.noreply.github.com>
2024-08-07 01:14:55 -0300

    Update Calculadora.js

--- Calculadora.js
+++ Calculadora.js
@@ -1,12 +1,5 @@
-    var operador;
-    var valor;
-    var valor2;
-    var readLineSync = require('readline-sync'); //tag readlineSync, permite que o script troque informações no console
-    var operador = readLineSync.question("Insira a Operacão que Deseja Efetuar (+) (-) (*) (/)? : \n");
-    var valor = parseFloat(reaLineSync.question("Insira o Numero: \n")); //a tag parseFloat converte uma string para numero
-    var valor2 = parseFloat(reaLineSync.question("Insira o Numero: \n"));
-    if(operador == "+"){
-        var operador = readLineSync.question(prompt("Digite a Operação Desejada"));
-        
-    }
-console.log('O resultado é', subt(operador, valor, valor2));
+var num;
+var operador;
+const readLineSync = require(readLineSync);
+const operador = readLineSync.question("Insira a Operacão que Deseja Efetuar (+) (-) (*) (/)? : \n");
+const num = parseFloat(reaLineSync.question("Insira o Numero: \n"));
