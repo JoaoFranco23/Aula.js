325fa0ab09756433be5f4760be5e8f1ea5121d10
JoaoFranco23 <147751659+JoaoFranco23@users.noreply.github.com>
2024-08-11 14:18:42 -0300

    Update and rename Calculadora.js to Subtração.js

--- Calculadora.js
+++
@@ -1,5 +0,0 @@
-var num;
-var operador;
-const readLineSync = require(readLineSync);
-const operador = readLineSync.question("Insira a Operacão que Deseja Efetuar (+) (-) (*) (/)? : \n");
-const num = parseFloat(reaLineSync.question("Insira o Numero: \n"));
---
+++ Subtração.js
@@ -0,0 +1,4 @@
+var subt = a, b;
+var a = 1000;
+var b = 200; 
+console.log(a-b);
